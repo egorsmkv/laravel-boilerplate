849b6b8771b183c1e006a352c859831f5691a55a
Yehor Smoliakov <yehors@ukr.net>
2024-01-02 08:47:34 +0200

    Update lang files to the latest versions #8

--- apps/frontend/resources/lang/en/auth.php
+++ apps/frontend/resources/lang/en/auth.php
@@ -1,11 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'failed'             => 'These credentials do not match our records.',
+    'password'           => 'The password is incorrect.',
     'social_auth_failed' => 'An error occurred while authorizing through a third-party resource.',
-
+    'throttle'           => 'Too many login attempts. Please try again in :seconds seconds.',
 ];
