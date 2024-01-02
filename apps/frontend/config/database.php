780ddf82623b87ead59c43dc44d1e3623167dfba
Yehor Smoliakov <yehors@ukr.net>
2024-01-02 09:22:01 +0200

    Add read/write hosts to PgSQL connection #11

--- apps/frontend/config/database.php
+++ apps/frontend/config/database.php
@@ -66,7 +66,17 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'read' => [
+                'host' => [
+                    env('DB_HOST_READ_1', '127.0.0.1'),
+                ],
+            ],
+            'write' => [
+                'host' => [
+                    env('DB_HOST', '127.0.0.1'),
+                ],
+            ],
+            // 'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
