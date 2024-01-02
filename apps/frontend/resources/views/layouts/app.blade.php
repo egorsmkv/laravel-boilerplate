1d6a0f25a09f6c9b9b3a5151dfc957214059d921
Yehor Smoliakov <yehors@ukr.net>
2024-01-02 14:47:37 +0200

    Move Flowbite logo to the repo

--- apps/frontend/resources/views/layouts/app.blade.php
+++ apps/frontend/resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <nav class="bg-white dark:bg-gray-900 w-full border-b border-gray-200 dark:border-gray-600">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="{{ route('home') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo">
+                <img src="{{ asset('icons/logo_flowbite.svg') }}" class="h-8" alt="Flowbite Logo">
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Laravel</span>
             </a>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
