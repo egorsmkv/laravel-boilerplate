81f8b2b111c93aeff3897f4b9306103279b474a0
Yehor Smoliakov <yehors+gh@ukr.net>
2024-01-10 23:14:21 +0200

    Add PHPStan #22

--- apps/frontend/app/Http/Requests/TestRequest.php
+++ apps/frontend/app/Http/Requests/TestRequest.php
@@ -6,6 +6,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class TestRequest extends FormRequest
 {
+    /**
+     * Retrieves an array of validation rules for the email field.
+     *
+     * @return array<string, array<string>> An array containing the validation rules for the email field.
+     */
     public function rules(): array
     {
         return [
