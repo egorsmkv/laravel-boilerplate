e2f5dc7147c9d0603a4df05d1281c11f7f5839dc
Yehor Smoliakov <yehors+gh@ukr.net>
2024-01-10 19:29:05 +0200

    A fix

--- apps/frontend/resources/views/layouts/app.blade.php
+++ apps/frontend/resources/views/layouts/app.blade.php
@@ -74,8 +74,10 @@
         <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
             <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
             <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                @if (app()->environment('local'))
+                @if (app()->isLocal())
                     <a href="/telescope" class="hover:underline">Telescope</a>
+                @else
+                    We are in production.
                 @endif
             </span>
             </div>
