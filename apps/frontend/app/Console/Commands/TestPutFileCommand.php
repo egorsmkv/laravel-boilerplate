86ee06600ccfc30eaeebc955389e9c2255a36230
Yehor Smoliakov <yehors+gh@ukr.net>
2024-01-15 22:10:28 +0200

    Add test command to put a file and tested in dev, #51

---
+++ apps/frontend/app/Console/Commands/TestPutFileCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Console\Command;
+
+class TestPutFileCommand extends Command
+{
+    protected $signature = 'app:test-put-file-command';
+    protected $description = 'Test command to put a file to the S3 storage';
+
+    public function handle(): void
+    {
+        $content = 'Some content';
+
+        $exists = Storage::disk('s3')->exists('first.txt');
+        if ($exists) {
+            $this->comment('The file exists.');
+            return;
+        }
+
+        $result = Storage::disk('s3')->put('first.txt', $content);
+
+        dump($result);
+
+        $this->comment('Result of upload is dumped.');
+
+        $fileURL = Storage::disk('s3')->url('first.txt');
+
+        $this->comment('The file URL:');
+        $this->comment($fileURL);
+    }
+}
