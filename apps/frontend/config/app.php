22ae62d6d3ea2a21d2dc034e2f4d57b433e5adcb
Yehor Smoliakov <yehors+gh@ukr.net>
2024-01-15 02:34:28 +0200

    Fixes, use Telescope only in dev

--- apps/frontend/config/app.php
+++ apps/frontend/config/app.php
@@ -165,10 +165,8 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
     ])->toArray(),
 
     /*
