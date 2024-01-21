727f388f0e1bbddc0abccbbd6a2dee49dd7c36de
Yehor Smoliakov <egorsmkv@gmail.com>
2024-01-21 11:22:34 +0000

    Remove laravel/sanctum #78

--- apps/frontend/app/Models/User.php
+++ apps/frontend/app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 /**
  * Class User.
@@ -23,7 +22,7 @@ use Laravel\Sanctum\HasApiTokens;
  */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
