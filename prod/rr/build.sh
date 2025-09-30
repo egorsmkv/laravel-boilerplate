242142d6b1f545ca2904abe9947d2487775d6474
Yehor Smoliakov <egorsmkv@gmail.com>
2025-09-30 15:34:17 +0300

    Some enhances

--- prod/rr/build.sh
+++ prod/rr/build.sh
@@ -4,6 +4,11 @@ export TIME=`date +%Y%m%d%H%M%S`
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 ./vx build -c velox.toml -o .
 
-strip -s rr
+if [ $? -ne 0 ]; then
+    echo "Velox build failed"
+    exit 1
+fi
 
-file rr
+strip -s ./rr
+
+file ./rr
